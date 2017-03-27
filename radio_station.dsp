a9d5f33e6975e16143b15b42270718f407a1738f
bi kai <bikai_xidian@163.com>
2017-03-28 06:14:22 +0800

    [W-M]2013.7.18, readio station code of PC with MFC.

--- radio_station.dsp
+++ radio_station.dsp
@@ -40,7 +40,6 @@ RSC=rc.exe
 # PROP Use_Debug_Libraries 0
 # PROP Output_Dir "Release"
 # PROP Intermediate_Dir "Release"
-# PROP Ignore_Export_Lib 0
 # PROP Target_Dir ""
 # ADD BASE CPP /nologo /MD /W3 /GX /O2 /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "_AFXDLL" /Yu"stdafx.h" /FD /c
 # ADD CPP /nologo /MD /W3 /GX /O2 /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "_AFXDLL" /D "_MBCS" /FR /Yu"stdafx.h" /FD /c
@@ -53,7 +52,7 @@ BSC32=bscmake.exe
 # ADD BSC32 /nologo
 LINK32=link.exe
 # ADD BASE LINK32 /nologo /subsystem:windows /machine:I386
-# ADD LINK32 SkinPPWTL.lib /nologo /subsystem:windows /machine:I386
+# ADD LINK32 /nologo /subsystem:windows /machine:I386
 
 !ELSEIF  "$(CFG)" == "radio_station - Win32 Debug"
 
@@ -66,7 +65,6 @@ LINK32=link.exe
 # PROP Use_Debug_Libraries 1
 # PROP Output_Dir "Debug"
 # PROP Intermediate_Dir "Debug"
-# PROP Ignore_Export_Lib 0
 # PROP Target_Dir ""
 # ADD BASE CPP /nologo /MDd /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_WINDOWS" /D "_AFXDLL" /Yu"stdafx.h" /FD /GZ /c
 # ADD CPP /nologo /MDd /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_WINDOWS" /D "_AFXDLL" /D "_MBCS" /FR /Yu"stdafx.h" /FD /GZ /c
@@ -79,7 +77,7 @@ BSC32=bscmake.exe
 # ADD BSC32 /nologo
 LINK32=link.exe
 # ADD BASE LINK32 /nologo /subsystem:windows /debug /machine:I386 /pdbtype:sept
-# ADD LINK32 SkinPPWTL.lib /nologo /subsystem:windows /debug /machine:I386 /pdbtype:sept
+# ADD LINK32 /nologo /subsystem:windows /debug /machine:I386 /pdbtype:sept
 
 !ENDIF 
 
@@ -90,10 +88,6 @@ LINK32=link.exe
 # Begin Group "Source Files"
 
 # PROP Default_Filter "cpp;c;cxx;rc;def;r;odl;idl;hpj;bat"
-# Begin Source File
-
-SOURCE=.\encrypt.cpp
-# End Source File
 # Begin Source File
 
 SOURCE=.\mscomm.cpp
@@ -121,10 +115,6 @@ SOURCE=.\StdAfx.cpp
 # PROP Default_Filter "h;hpp;hxx;hm;inl"
 # Begin Source File
 
-SOURCE=.\encrypt.h
-# End Source File
-# Begin Source File
-
 SOURCE=.\mscomm.h
 # End Source File
 # Begin Source File
@@ -141,20 +131,12 @@ SOURCE=.\Resource.h
 # End Source File
 # Begin Source File
 
-SOURCE=.\SkinPPWTL.h
-# End Source File
-# Begin Source File
-
 SOURCE=.\StdAfx.h
 # End Source File
 # End Group
 # Begin Group "Resource Files"
 
 # PROP Default_Filter "ico;cur;bmp;dlg;rc2;rct;bin;rgs;gif;jpg;jpeg;jpe"
-# Begin Source File
-
-SOURCE=.\res\cursor1.cur
-# End Source File
 # Begin Source File
 
 SOURCE=.\res\icon1.ico
--- radio_stationDlg.h
+++ radio_stationDlg.h
@@ -21,17 +21,9 @@ public:
 	CRadio_stationDlg(CWnd* pParent = NULL);	// standard constructor
 	HICON m_hIconRed;    //串口打开时的红灯图标句柄
 	HICON m_hIconOff;    //串口关闭时的指示图标句柄
-	afx_msg LRESULT OnShowTask(WPARAM wParam,LPARAM lParam);
-
 // Dialog Data
 	//{{AFX_DATA(CRadio_stationDlg)
 	enum { IDD = IDD_RADIO_STATION_DIALOG };
-	CStatic	m_board_led_YW;
-	CStatic	m_ctrlIconOpenoff_YW;
-	CComboBox	m_Com_YW;
-	CSliderCtrl	m_POWER_SELECT;
-	CListCtrl	m_rssi_list;
-	CComboBox	m_alarm_command;
 	CStatic	m_board_connect;
 	CStatic	m_board_led;
 	CStatic	m_ctrlIconOpenoff;
@@ -43,14 +35,13 @@ public:
 	int		m_radio_wakeup;
 	CMSComm	m_comm;
 	double	m_radio_id;
+	double	m_frame_counter;
 	int		m_wakeup_time;
 	double	m_frequency;
 	double	m_frequency_native;
 	double	m_unicast_terminal_id;
 	double	m_multi_terminal_id_start;
 	double	m_multi_terminal_id_end;
-	CString	m_power_num;
-	CMSComm	m_comm_YW;
 	//}}AFX_DATA
 
 	// ClassWizard generated virtual function overrides
@@ -90,88 +81,36 @@ protected:
 	afx_msg void OnKillfocusEditUnicast();
 	afx_msg void OnKillfocusEditMulticastStart();
 	afx_msg void OnKillfocusEditMulticastEnd();
-	afx_msg void OnSelendokComboAlarmType();
-	afx_msg void OnButtonAlarm();
-	afx_msg void OnKillfocusEditBoardFrequency();
-	afx_msg void OnButtonScan();
-	afx_msg void OnTimer(UINT nIDEvent);
-	afx_msg void OnClose();
-	afx_msg void OnDestroy();
-	afx_msg void OnButtonVoice();
-	afx_msg void OnButtonIdentify();
-	afx_msg void OnButtonBoardReset();
-	afx_msg void OnButtonTTS();
-	afx_msg void OnButtonNMIC();
-	afx_msg void OnButtonAdvanced();
-	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
-	afx_msg void OnCancelMode();
-	afx_msg void OnReleasedcaptureSliderPower(NMHDR* pNMHDR, LRESULT* pResult);
-	afx_msg void OnButtonConnect_YW();
-	afx_msg void OnComm_YW();
-	afx_msg void OnEditchangeComboComselectYw();
-	afx_msg void OnSelendokComboComselectYw();
 	DECLARE_EVENTSINK_MAP()
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
 
 public:
-	bool flag_fre_is_scaning;//正在扫描频谱
-	int timer_board_disconnect_times;//定时器3统计尝试连接次数，达到3次则判断子板未连接
-	int timer_board_disconnect_times_YW;//定时器6统计尝试连接次数，达到3次则判断运维板未连接
-//	LRESULT OnShowTask( WPARAM wParam,LPARAM lParam );
-	void ToTray();
-	void four_bits_ASCII(unsigned char* a,unsigned char* b,int len,int index);
-	void int_bits(int a, unsigned char* b, int len);
 	unsigned char XOR(unsigned char *BUFF, int len);
 	unsigned char frame_type[2];//帧类型:01表示本帧是唤醒帧、10代表控制帧、11代表认证数据帧、00代表认证续传帧
 	unsigned char wakeup_type[2];//唤醒帧类型:单播01、广播10、组播11
 	unsigned char communication_fre_point[8];//通信频点：本标志位的值表示对87MHz以100KHz为单位的偏移量
 	unsigned char source_address[36];//源地址：电台的ID，电台的电话卡转换的二进制串，由于存在跨区用户也要收听应急信息，所以该标志位作为中端的一个参考
 	unsigned char target_address_unicast[24];//单播目标地址
-	unsigned char target_address_multicast_start[24];//多播目标起始地址
-	unsigned char target_address_multicast_end[24];//多播目标终止地址
+	unsigned char target_address_multicast[48];//多播目标地址
 	unsigned char frame_counter[36];//帧计数器
 	unsigned char MAC_AES[36];//AES后缀
-
-	int	m_frame_counter;//帧计数器
-	int terminal_control_index;//发送给终端的控制索引
 	
 //	unsigned char
 
 private:
-	int m_DCom;//广播板串口号
-	int m_DCom_YW;//运维板串口号
+	int m_DCom;
 	int m_DStopbits;
 	char m_DParity;
 	int m_DDatabits;
 	LONG m_DBaud;
-	int alarm_index;
 	
 	BOOL SerialPortOpenCloseFlag;//串口打开关闭标志位
-	BOOL SerialPortOpenCloseFlag_YW;//运维串口打开关闭标志位
 	bool flag_modified;//修改唤醒帧字段标志位
-	bool flag_com_init_ack;//上位机软件查询下位机，下位机对查询信息的应答标志位。1:连接成功；0：连接失败；
-	bool flag_com_init_ack_YW;//上位机软件查询运维板，运维板对查询信息的应答标志位。1:连接成功；0：连接失败；
+	bool flag_com_init_ack;//上位机软件查询下位机，下位机对查询信息的应答标志位
 	int frame_index;//接收缓冲帧的索引
-	int frame_index_YW;//运维串口接收缓冲帧的索引
-	int frame_send_index;//发送缓冲区无线帧比特流计数器
-	int frame_board_send_index;//子板通信数据帧计数器
-	int index_frequency_point;//下位机反馈频点计数器
-//	int index_before_gray;//格雷编码前数组索引
-	int index_after_gray;//格雷编码后数组索引
-	unsigned char index_wakeup_times;//连接帧发送次数计数器，上下位机通信，保证每帧数据都不同
-	unsigned char index_scan_times;//频谱扫描帧发送次数计数器，上下位机通信，保证每帧数据都不同
-	unsigned char index_control_times;//控制帧发送次数计数器，上下位机通信，保证每帧数据都不同
-	unsigned char index_data_times;//数据帧发送次数计数器，上下位机通信，保证每帧数据都不同
+	unsigned char index_wakeup_times;//唤醒次数计数器
 	bool flag_board_modified;//修改下位机配置标志位
-//	bool flag_scan_button;//扫描按钮上文字切换
-	int index_resent_data_frame;//重传帧编号，0：空闲(查询帧：查询帧不使用出错重传机制，因为是不停的查询的)；1~3：唤醒帧编号；4：报警帧编号；5：认证帧编号；6：运维板复位帧编号；7：频谱扫描，继电器控制帧；
-
-	CStatusBarCtrl *m_StatBar;//状态栏
-	bool flag_voice_broad;//软件界面上开始广播停止广播文字切换标志位
-	int voice_broad;//语音广播标志位3：开始广播；4：停止广播；
-
-	bool flag_button_advanced;//“高级”按钮。1：按下；0：弹起；
 
 };
 
